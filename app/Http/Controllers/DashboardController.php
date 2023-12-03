883a4b27e8c0e6590c8f608a5ce463b1de5000a8
lahkokaku <jose.satrya@gmail.com>
2023-12-04 00:04:54 +0700

    feat: added admin dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\TransactionHeader;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Date;
 
 class DashboardController extends Controller
 {
@@ -11,6 +14,40 @@ class DashboardController extends Controller
     }
 
     public function admin(){
-        return view('dashboards.admin');
+
+        $month = Carbon::now()->month;
+        $year = Carbon::now()->year;
+
+
+        // Monthly Data
+        $onGoingTransactionCountThisMonth = TransactionHeader::where('status', '!=', '4')->count();
+        $finishedTransactionsThisMonth = TransactionHeader::where('status', '=', '4')
+                                ->where('transaction_date', '>=', $year.'-'.$month.'-'.'01')
+                                ->where('transaction_date', '<=', $year.'-'.$month.'-'.'31')
+                                ->get();
+        $revenueThisMonth = 0;
+
+        foreach($finishedTransactionsThisMonth as $finishedTransaction){
+            $revenueThisMonth += $finishedTransaction->total_price;
+        }
+
+        // Annual Data
+        $finishedTransactionsThisYear = TransactionHeader::where('status', '=', '4')
+                                ->where('transaction_date', '>=', $year.'-01-'.'01')
+                                ->where('transaction_date', '<=', $year.'-12-'.'31')
+                                ->get();
+        $revenueThisYear = 0;
+
+        foreach($finishedTransactionsThisYear as $finishedTransaction){
+            $revenueThisYear += $finishedTransaction->total_price;
+        }
+
+        return view('dashboards.admin',[
+            "onGoingTransactionCountThisMonth" => $onGoingTransactionCountThisMonth,
+            "finishedTransactionCountThisMonth" => $finishedTransactionsThisMonth->count(),
+            "revenueThisMonth" => $revenueThisMonth,
+            "finishedTransactionCountThisYear" => $finishedTransactionsThisYear->count(),
+            "revenueThisYear" => $revenueThisYear
+        ]);
     }
 }
--- resources/views/auth/login-admin.blade.php
+++ resources/views/auth/login-admin.blade.php
@@ -51,7 +51,7 @@
     
                             <div class="row mb-0">
                                 <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn btn-primary">
+                                    <button type="submit" class="btn btn-blue-3">
                                         {{ __('Login') }}
                                     </button>
                                 </div>
